810849d3b25575511e2f51f26b679fdddec93453
Najeeb <najeeb.nedo5555@gmail.com>
2025-02-18 17:57:34 +0500

    Coming Soon

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Coming Soon v6.0.7 (https://startbootstrap.com/theme/coming-soon)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+* Start Bootstrap - RockMerchTT - Coming Soon v6.0.7 (https://rockmerchtt.com)
+* Copyright 2013-2025 RockMerchTT
+* Licensed under MIT (https://github.com/StartBootstrap/rockmerchtt-coming-soon/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
